c717bb4954994b07271d18f6726ea4a4f0612a7e
George Okereka <georgeokez@gmail.com>
2018-11-28 23:21:38 +0100

    Initial commit

---
+++ src/main/java/com/george/dev/bioenergywebiste/BioEnergyWebisteApplication.java
@@ -0,0 +1,12 @@
+package com.george.dev.bioenergywebiste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioEnergyWebisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioEnergyWebisteApplication.class, args);
+    }
+}
