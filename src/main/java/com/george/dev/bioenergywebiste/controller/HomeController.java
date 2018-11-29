8a2be0c26622badeb6dfd184d1a0a834d236e2fb
George Okereka <georgeokez@gmail.com>
2018-11-29 12:06:13 +0100

    added asses to the project

---
+++ src/main/java/com/george/dev/bioenergywebiste/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.george.dev.bioenergywebiste.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"/","/index","/home"})
+    public String homePage(){
+        return "index";
+    }
+}
